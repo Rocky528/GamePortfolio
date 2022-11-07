9fea662f16ee5f2887f57b8cb2e2f9ad5a0f5aa0
Rocky528 <aokinagashi@gmail.com>
2022-11-07 11:29:22 -0500

    init commit

---
+++ src/parts/BrandIcon.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+import Button from 'elements/Button';
+
+export default function BrandIcon() {
+  return (
+    <Button
+      className=""
+      type="link"
+      href="/"
+    >
+      <p className="text-theme-blue text-4xl">
+        ATOMOTOS
+        <span className="text-theme-purple"> Dream Come true</span>
+      </p>
+    </Button>
+  );
+}
